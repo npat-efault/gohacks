010d4d52834b61b537bf86930a1ce63a2e6bc58c
Nick Patavalis <npat@efault.net>
2015-10-22 13:09:29 +0300

    cirq: Improved code generation
    Improved code generation script "cirq_gen.sh":
    - Better error handling
    - Look for the input file (cirq.gox) first in current directory
      and then in the package dir.
    - Make generated file read-only
    
    Removed Makefile. Now code is generated using "go generate"

--- cirq_gen.sh
+++ cirq_gen.sh
@@ -1,26 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $# -ne 4 ]; then
-    echo "Usage is: $(basename $0) <pkg> <qtype> <new> <el>" 1>&2
+if [ $# -ne 5 ]; then
+    echo "Usage is: $(basename $0) <outfile> <pkg> <qtype> <new> <el>" 1>&2
+    echo "    <outfile> is the name of the generated file" 1>&2
     echo "    <pkg> is the package name for the generated file" 1>&2
     echo "    <qtype> is the name for the queue type" 1>&2
     echo "    <new> is the name for the function returning a new queue" 1>&2
     echo "    <el> is the type for queue elements" 1>&2
     exit 1
-fi 
-
-package=$1
-qtype=$2
-new=$3
-eltype=$4
-
-src=$(go list -f '{{ .Dir }}' "github.com/npat-efault/gohacks/cirq")/cirq.gox
-if [ ! -f "$src" ]; then
-    echo "Not found: $src" 1>&2
-    exit 1
 fi
 
-cat <<EOF
+outfile="$1"
+package=$2
+qtype=$3
+new=$4
+eltype=$5
+
+gox="cirq.gox"
+src="./$gox"
+pkg="github.com/npat-efault/gohacks/cirq"
+if [ ! -f "$src" ]; then
+    src=$(go list -f '{{ .Dir }}' "$pkg")/"$gox"
+    if [ ! -f "$src" ]; then
+        echo "Cannot find \"$gox\" in . or in package \"$pkg\"" 1>&2
+        exit 1
+    fi
+fi
+
+set -e
+trap 'rm -f "$outfile".tmp' EXIT
+
+cat > "$outfile".tmp <<EOF
 // Auto-generated. !! DO NOT EDIT !!
 
 EOF
@@ -29,4 +39,7 @@ sed \
     -e "s/__Q/$qtype/g" \
     -e "s/__NewQ/$new/g" \
     -e "s/__ELTYPE/$eltype/g" \
-    "$src" | gofmt 
+    "$src" | gofmt >> "$outfile".tmp
+mv "$outfile".tmp "$outfile"
+chmod -w "$outfile"
+
--- doc.go
+++ doc.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2014, Nick Patavalis (npat@efault.net).
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
+
 // Package cirq provides a circular double-ended queue
 // implementation. The implementation is based on slices and supports
 // the typical PushFront / PushBack, PopFront / PopBack, PeekFront /
@@ -5,7 +10,9 @@
 // elements of type interface{}.
 //
 // You can generate queue implementations specialized to specific
-// element data-types using the cirq_gen.sh script. See Makefile for
-// an example.
+// element data-types using the cirq_gen.sh script. See "doc.go" in
+// the package sources for an example.
 //
 package cirq
+
+//go:generate ./cirq_gen.sh cirq.go cirq CQ New "interface{}"
